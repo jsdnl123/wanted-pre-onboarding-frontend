8f831c8a21965722f11aedf5711d81a522b115b3
위진세 <dnlwlstp123@gmail.com>
2023-02-10 14:26:12 +0900

    기본 Page 생성, Router 설정, 확장자 jsx로 변경

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Todo from "./pages/Todo";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
